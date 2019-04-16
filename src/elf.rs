759f195ff20deb4e21b6d4a9d007c98fefb88331
SyuJyo <jayong93@gmail.com>
2019-04-16 09:37:17 +0900

    elf 모듈 구현중

--- src/elf.rs
+++ src/elf.rs
@@ -1,39 +1,51 @@
-use goblin::elf;
 use byteorder::*;
+use goblin::elf;
 
 #[derive(Debug, Default)]
 pub struct Section {
-    address: usize,
+    base_addr: usize,
     size: usize,
-    data: Vec<u32>
+    data: Vec<u8>,
 }
 
-fn text_section(elf: &elf::Elf, data: &Vec<u8>) -> Option<Section> {
-    elf.section_headers.iter()
-        .inspect(|header| {eprintln!("{:?}", header);})
+#[allow(dead_code)]
+fn extract_section(
+    elf: &elf::Elf,
+    _data: &Vec<u8>,
+    name: &str,
+) -> Option<Section> {
+    elf.section_headers
+        .iter()
         .filter(|header| header.sh_type == 1)
-        .filter(|header| elf.shdr_strtab.get(header.sh_name)
+        .inspect(|header| {
+            eprintln!("{:?}", header);
+        })
+        .filter(|header| {
+            elf.shdr_strtab
+                .get(header.sh_name)
                 .filter(|res| {
                     res.as_ref()
                         .ok()
-                        .filter(|name| {
-                            eprintln!("{}", name);
-                            name == &&".text"
+                        .filter(|n| {
+                            eprintln!("{}", n);
+                            **n == name
                         })
                         .is_some()
                 })
-                .is_some())
-        .map(|header| Section {
-            address: header.sh_addr as usize,
-            size: header.sh_size as usize,
-            data: data.as_slice().chunks_exact(4).map(|chunk| {
-                if elf.little_endian {
-                    chunk.read_u32::<LittleEndian>().unwrap()
-                } else {
-                    chunk.read_u32::<BigEndian>().unwrap()
-                }
-            }).collect()
-        }).take(1).collect::<Vec<_>>().pop()
+                .is_some()
+        })
+        .map(|header| {
+            let base_addr = header.sh_addr as usize;
+            let size = header.sh_size as usize;
+            Section {
+                base_addr,
+                size,
+                ..Default::default()
+            }
+        })
+        .take(1)
+        .collect::<Vec<_>>()
+        .pop()
 }
 
 #[cfg(test)]
@@ -43,14 +55,18 @@ mod tests {
     #[test]
     fn text_section_address_test() {
         use std::fs::*;
-        use std::io::{Read};
+        use std::io::Read;
 
-        let mut file = File::open(concat!(env!("CARGO_MANIFEST_DIR"), "/src/tests/hello")).unwrap();
+        let mut file = File::open(concat!(
+            env!("CARGO_MANIFEST_DIR"),
+            "/src/tests/hello"
+        ))
+        .unwrap();
         let mut bytes: Vec<u8> = Vec::new();
-        file.read_to_end(&mut bytes);
+        file.read_to_end(&mut bytes).unwrap();
         let elf = elf::Elf::parse(&bytes).unwrap();
-        let t_section = text_section(&elf, &bytes).unwrap();
-        assert_eq!(t_section.address, 0x400680);
-        assert_eq!(t_section.size, 0x1e2);
+        let t_section = extract_section(&elf, &bytes, ".text").unwrap();
+        assert_eq!(t_section.base_addr, 0x10114);
+        assert_eq!(t_section.size, 0x51830);
     }
 }
