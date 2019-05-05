b6bd6e70266297a9904a7227c755071c5e4cc91a
Jae Yong Choi <jayong93@gmail.com>
2019-05-05 22:47:24 +0900

    implementing write functions to memory

--- src/memory/mod.rs
+++ src/memory/mod.rs
@@ -10,35 +10,70 @@ use std::mem::size_of;
 use std::ops::Range;
 
 #[derive(Debug, Default)]
-pub struct InstructionMemory {
-    // virtual address range
+pub struct ProcessMemory {
     v_address_range: (u32, u32),
+    read_only_range: (u32, u32),
     data: Vec<u8>,
-    entry_point: u32,
 }
 
-impl InstructionMemory {
-    pub fn read(&self, addr: u32) -> u32 {
+impl ProcessMemory {
+    pub fn new(elf_struct: &goblin::elf::Elf, elf_data: &[u8]) -> Self {
+        elf_struct
+            .program_headers
+            .iter()
+            .filter(|header| header.p_type == goblin::elf::program_header::PT_LOAD)
+            .fold(ProcessMemory::default(), |mut memory, header| {
+                    let vm_range = header.vm_range();
+                    if !header.is_write() {
+                        if memory.read_only_range.0 == memory.read_only_range.1 {
+                            memory.read_only_range.0 = vm_range.start as u32;
+                            memory.read_only_range.1 = vm_range.end as u32;
+                        } else {
+                            memory.read_only_range.1 = vm_range.end as u32;
+                        }
+                    }
+
+                    if memory.v_address_range.0 == memory.v_address_range.1 {
+                        memory.v_address_range.0 = vm_range.start as u32;
+                        memory.v_address_range.1 = vm_range.end as u32;
+                    } else {
+                        let old_size = memory.data.len();
+                        if memory.v_address_range.1 < vm_range.start as u32 {
+                            memory.data.resize(old_size + (vm_range.start as u32 - memory.v_address_range.1) as usize, 0);
+                        }
+                        memory.v_address_range.1 = vm_range.end as u32;
+                    }
+                    let old_size = memory.data.len();
+                    memory.data.resize(old_size + (header.p_memsz as usize), 0);
+                    memory.data[old_size..(old_size + header.p_filesz as usize)]
+                        .copy_from_slice(&elf_data[header.file_range()]);
+                    memory
+                },
+            )
+    }
+
+    fn check_address_space(&self, addr: u32) {
         if addr < self.v_address_range.0 || addr >= self.v_address_range.1 {
-            panic!("{} is out of instruction address range.", addr);
+            panic!("{} is out of address range.", addr);
         }
+    }
+    
+    fn check_write_address_space(&self, addr: u32) {
+        if self.read_only_range.0 <= addr && addr < self.read_only_range.1 {
+            panic!("{} is out of writable address range.", addr);
+        }
+    }
+
+    pub fn read_inst(&self, addr: u32) -> u32 {
+        self.check_address_space(addr);
+
         let offset = (addr - self.v_address_range.0) as usize;
         let mut data = &(self.data[offset..offset+4]);
         data.read_u32::<LittleEndian>().expect("Can't read memory as u32 instruction")
     }
-}
-
-#[derive(Debug, Default)]
-pub struct DataMemory {
-    v_address_range: (u32, u32),
-    data: Vec<u8>,
-}
-
-impl DataMemory {
-    pub fn read_int<T: ReadBytesExt + num_traits::PrimInt + num_traits::FromPrimitive>(&self, addr: u32) -> T {
-        if addr < self.v_address_range.0 || addr >= self.v_address_range.1 {
-            panic!("{} is out of data address range.", addr);
-        }
+    
+    pub fn read_int<T: num_traits::PrimInt + num_traits::FromPrimitive>(&self, addr: u32) -> T {
+        self.check_address_space(addr);
         
         let offset = (addr - self.v_address_range.0) as usize;
         let data_size = size_of::<T>() as usize;
@@ -48,58 +83,36 @@ impl DataMemory {
     }
 
     pub fn read_float(&self, addr: u32) -> f32 {
-        if addr < self.v_address_range.0 || addr >= self.v_address_range.1 {
-            panic!("{} is out of data address range.", addr);
-        }
+        self.check_address_space(addr);
+
         let offset = (addr - self.v_address_range.0) as usize;
         let mut data = &(self.data[offset..offset+4]);
         data.read_f32::<LittleEndian>().expect("Can't read a memory as u64")
     }
-}
 
-pub fn get_image_from_elf(elf: &[u8]) -> (InstructionMemory, DataMemory) {
-    let elf_struct =
-        goblin::elf::Elf::parse(elf).expect("It's not elf format data.");
-    elf_struct
-        .program_headers
-        .iter()
-        .filter(|header| header.p_type == goblin::elf::program_header::PT_LOAD)
-        .fold(
-            (
-                InstructionMemory {
-                    entry_point: elf_struct.entry as u32,
-                    ..Default::default()
-                },
-                DataMemory::default(),
-            ),
-            |(mut inst, mut data), header| {
-                let vm_range = header.vm_range();
-                if header.is_write() {
-                    if data.v_address_range.0 == data.v_address_range.1 {
-                        data.v_address_range.0 = vm_range.start as u32;
-                        data.v_address_range.1 = vm_range.end as u32;
-                    } else {
-                        data.v_address_range.1 = vm_range.end as u32;
-                    }
-                    let old_size = data.data.len();
-                    data.data.resize(header.p_memsz as usize, 0);
-                    data.data[old_size..(old_size + header.p_filesz as usize)]
-                        .copy_from_slice(&elf[header.file_range()]);
-                } else if header.is_read() {
-                    if inst.v_address_range.0 == inst.v_address_range.1 {
-                        inst.v_address_range.0 = vm_range.start as u32;
-                        inst.v_address_range.1 = vm_range.end as u32;
-                    } else {
-                        inst.v_address_range.1 = vm_range.end as u32;
-                    }
-                    let old_size = inst.data.len();
-                    inst.data.resize(header.p_memsz as usize, 0);
-                    inst.data[old_size..(old_size + header.p_filesz as usize)]
-                        .copy_from_slice(&elf[header.file_range()]);
-                }
-                (inst, data)
-            },
-        )
+    pub fn write_int<T: num_traits::PrimInt + num_traits::AsPrimitive<u64>>(&mut self, addr: u32, value: T) {
+        self.check_address_space(addr);
+        self.check_write_address_space(addr);
+
+        let offset = (addr-self.v_address_range.0) as usize;
+        let data_size = size_of::<T>() as usize;
+        let mut data = &mut (self.data[offset..offset+data_size]);
+        // transmute 사용해서 변경
+        match data_size {
+            1 => 
+        }
+        println!("{}", value.as_());
+        data.write_uint::<LittleEndian>(value.as_(), data_size).unwrap();
+    }
+
+    pub fn write_float(&mut self, addr: u32, value: f32) {
+        self.check_address_space(addr);
+        self.check_write_address_space(addr);
+
+        let offset = (addr-self.v_address_range.0) as usize;
+        let mut data = &mut (self.data[offset..offset+4]);
+        data.write_f32::<LittleEndian>(value).unwrap();
+    }
 }
 
 #[cfg(test)]
@@ -113,203 +126,65 @@ mod tests {
             .unwrap()
             .read_to_end(&mut elf_data)
             .unwrap();
-        let (inst_image, data_image) = get_image_from_elf(&elf_data);
-        assert_eq!(inst_image.v_address_range.0, 0x10000);
-        assert_eq!(inst_image.v_address_range.1, 0x10000 + 0x6dd46);
-        assert_eq!(inst_image.entry_point, 0x10338);
-        assert_eq!(inst_image.data.len(), 0x6dd46);
-        assert_eq!(inst_image.read(inst_image.entry_point), 0x034000ef);
-        assert_eq!(data_image.v_address_range.0, 0x7fd08);
-        assert_eq!(data_image.v_address_range.1, 0x7fd08 + 0x1e0c);
-        assert_eq!(data_image.data.len(), 0x1e0c);
+        let elf = goblin::elf::Elf::parse(&elf_data).unwrap();
+        let image = ProcessMemory::new(&elf, &elf_data);
+        let entry_point = elf.entry as u32;
+        assert_eq!(image.v_address_range.0, 0x10000);
+        assert_eq!(image.v_address_range.1, 0x7fd08 + 0x1e0c);
+        assert_eq!(image.read_only_range.0, 0x10000);
+        assert_eq!(image.read_only_range.1, 0x10000+0x6dd46);
+        assert_eq!(entry_point, 0x10338);
+        assert_eq!(image.data.len(), 0x7fd08+0x1e0c - 0x10000);
+    }
+
+    #[test]
+    fn test_reading_memory() {
+        let mut elf_data = Vec::new();
+        let _ = std::fs::File::open("tests/hello")
+            .unwrap()
+            .read_to_end(&mut elf_data)
+            .unwrap();
+        let elf = goblin::elf::Elf::parse(&elf_data).unwrap();
+        let image = ProcessMemory::new(&elf, &elf_data);
+        let entry_point = elf.entry as u32;
+
+        assert_eq!(image.read_inst(entry_point), 0x034000ef);
+        let str_bytes = (0x625b0..0x625b0+13).map(|addr| image.read_int::<u8>(addr)).collect::<Vec<u8>>();
+        assert_eq!(String::from_utf8(str_bytes).unwrap(), "Hello, World!");
+    }
+
+    #[test]
+    fn test_writing_memory() {
+        let mut elf_data = Vec::new();
+        let _ = std::fs::File::open("tests/hello")
+            .unwrap()
+            .read_to_end(&mut elf_data)
+            .unwrap();
+        let elf = goblin::elf::Elf::parse(&elf_data).unwrap();
+        let mut image = ProcessMemory::new(&elf, &elf_data);
+
+        image.write_int(0x7fd08, b'A');
+        let result: char = image.read_int::<u8>(0x7fd08).into();
+        assert_eq!(result, 'A');
+        image.write_int(0x80000, 100u32);
+        assert_eq!(image.read_int::<u32>(0x80000), 100);
+        image.write_int(0x80000, -10i8);
+        assert_eq!(image.read_int::<i8>(0x80000), -10);
+        //image.write_float(0x80000, 3.14);
+        //assert_eq!(image.read_float(0x80000), 3.14);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_writing_to_read_only_memory() {
+        let mut elf_data = Vec::new();
+        let _ = std::fs::File::open("tests/hello")
+            .unwrap()
+            .read_to_end(&mut elf_data)
+            .unwrap();
+        let elf = goblin::elf::Elf::parse(&elf_data).unwrap();
+        let mut image = ProcessMemory::new(&elf, &elf_data);
+
+        image.write_int(0x20000, 100);
     }
 }
-// pub struct RawSection {
-//     range: Range<u32>,
-//     data: Vec<u8>,
-// }
-
-// impl RawSection {
-//     pub fn read_int<T: num_traits::PrimInt>(&self, addr: u32) -> T {
-//         let size = size_of::<T>();
-//         let start_addr = (addr-self.range.start) as usize;
-//         let mut num = &self.data[(start_addr..start_addr+size)];
-//         match size {
-//             1 => num.read_u8().map(|n| T::from(n).unwrap()).expect("Can't convert to byte"),
-//             2 => num.read_u16::<LittleEndian>().map(|n| T::from(n).unwrap()).expect("Can't convert to half"),
-//             4 => num.read_u32::<LittleEndian>().map(|n| T::from(n).unwrap()).expect("Can't convert to word"),
-//             8 => num.read_u64::<LittleEndian>().map(|n| T::from(n).unwrap()).expect("Can't convert to dword"),
-//             _ => unreachable!()
-//         }
-//     }
-
-//     pub fn read_float(&self, addr: u32) -> f32 {
-//         let start_addr = (addr-self.range.start) as usize;
-//         let mut num = &self.data[(start_addr..start_addr+4)];
-//         num.read_f32::<LittleEndian>().expect("Can't convert to float")
-//     }
-
-//     pub fn write_float(&mut self, addr: u32, value: f32) {
-//         let start_addr = (addr - self.range.start) as usize;
-//         let mut num = &mut self.data[(start_addr..start_addr+4)];
-//         num.write_f32::<LittleEndian>(value).unwrap();
-//     }
-
-//     pub fn write_int<T: num_traits::PrimInt>(&mut self, addr: u32, value: T) {
-//         let size = size_of::<T>();
-//         let start_addr = (addr-self.range.start) as usize;
-//         let mut num = &mut self.data[(start_addr..start_addr+size)];
-//         match size {
-//             1 => num.write_u8(value.to_u8().unwrap()),
-//             2 => num.write_u16::<LittleEndian>(value.to_u16().unwrap()),
-//             4 => num.write_u32::<LittleEndian>(value.to_u32().unwrap()),
-//             8 => num.write_u64::<LittleEndian>(value.to_u64().unwrap()),
-//             _ => unreachable!()
-//         }.unwrap();
-//     }
-// }
-
-// pub enum Section {
-//     Execute(RawSection),
-//     Normal(RawSection),
-// }
-
-// impl Section {
-//     pub fn read_int<T: num_traits::PrimInt>(&self, addr: u32) -> T {
-//         match self {
-//             Section::Execute(s) | Section::Normal(s) => s.read_int(addr),
-//         }
-//     }
-//     pub fn read_float(&self, addr: u32) -> f32 {
-//         match self {
-//             Section::Execute(s) | Section::Normal(s) => s.read_float(addr),
-//         }
-//     }
-//     pub fn write_int<T: num_traits::PrimInt>(&mut self, addr: u32, value: T) {
-//         match self {
-//             Section::Execute(s) | Section::Normal(s) => s.write_int(addr, value),
-//         }
-//     }
-//     pub fn write_float(&mut self, addr: u32, value: f32) {
-//         match self {
-//             Section::Execute(s) | Section::Normal(s) => s.write_float(addr, value),
-//         }
-//     }
-// }
-
-// pub struct ProcessMemory {
-//     sections: Vec<Section>,
-//     entry_point: u32,
-//     // Stack 자료구조 추가
-// }
-
-// impl ProcessMemory {
-//     pub fn new(mut file: impl Read) -> Self {
-//         let mut file_data = Vec::new();
-//         file.read_to_end(&mut file_data).expect("Can't read data");
-//         let elf =
-//             goblin::elf::Elf::parse(&file_data).expect("Can't parse data");
-//         let sections = elf
-//             .section_headers
-//             .iter()
-//             .filter(|header| header.is_alloc())
-//             .map(|header| {
-//                 if header.is_executable() {
-//                     let range = header.vm_range();
-//                     let data = file_data[header.file_range()].to_vec();
-//                     Section::Execute(RawSection {
-//                         range: (range.start as u32..range.end as u32),
-//                         data,
-//                     })
-//                 } else if header.sh_type
-//                     == goblin::elf::section_header::SHT_NOBITS
-//                 {
-//                     let range = header.vm_range();
-//                     let mut data = Vec::new();
-//                     data.resize_with(
-//                         range.end - range.start,
-//                         Default::default,
-//                     );
-//                     Section::Normal(RawSection {
-//                         range: (range.start as u32..range.end as u32),
-//                         data,
-//                     })
-//                 } else {
-//                     let range = header.vm_range();
-//                     let data = file_data[header.file_range()].to_vec();
-//                     Section::Normal(RawSection {
-//                         range: (range.start as u32..range.end as u32),
-//                         data,
-//                     })
-//                 }
-//             })
-//             .collect();
-//         ProcessMemory { sections, entry_point: elf.entry as u32 }
-//     }
-
-//     pub fn read_int<T: num_traits::PrimInt>(&self, addr: u32) -> T {
-//         let sec = self.get_section(addr).expect("Wrong address");
-//         sec.read_int::<T>(addr)
-//     }
-
-//     pub fn read_float(&self, addr: u32) -> f32 {
-//         let sec = self.get_section(addr).expect("Wrong address");
-//         sec.read_float(addr)
-//     }
-
-//     pub fn write_int<T: num_traits::PrimInt>(&mut self, addr: u32, value: T) {
-//         let sec = self.get_section_mut(addr).expect("Wrong address");
-//         sec.write_int(addr, value)
-//     }
-
-//     pub fn write_float(&mut self, addr: u32, value: f32) {
-//         let sec = self.get_section_mut(addr).expect("Wrong address");
-//         sec.write_float(addr,value)
-//     }
-
-//     pub fn fetch_instruction(&self, addr: u32) -> u32 {
-//         let sec = self.get_section(addr);
-
-//         match sec {
-//             Ok(section) => {
-//                 if addr % 4 != 0 {panic!("Not aligned address.")}
-//                 if let Section::Execute(ref s) = section {
-//                     s.read_int::<u32>(addr)
-//                 }
-//                 else {
-//                     panic!("The address is not in executable instruction section.")
-//                 }
-//             },
-//             Err(_) => panic!("The address is not in process's memory area."),
-//         }
-//     }
-
-//     fn get_section(&self, addr: u32) -> Result<&Section, usize> {
-//         let sec = self.find_section_index(addr);
-//         sec.map(|idx| {
-//             &self.sections[idx]
-//         })
-//     }
-
-//     fn get_section_mut(&mut self, addr: u32) -> Result<&mut Section, usize> {
-//         let sec = self.find_section_index(addr);
-//         sec.map(move |idx| {
-//             &mut self.sections[idx]
-//         })
-//     }
-
-//     fn find_section_index(&self, addr: u32) -> Result<usize, usize> {
-//         self.sections.binary_search_by(|section| {
-//             let range = match section {
-//                 Section::Execute(s) | Section::Normal(s) => &s.range,
-//             };
-//             if addr < range.start {
-//                 Ordering::Less
-//             } else if addr >= range.end {
-//                 Ordering::Greater
-//             } else {
-//                 Ordering::Equal
-//             }
-//         })
-//     }
-// }
